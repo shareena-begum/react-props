b395b791efefe50f2a92f9edf0ecb0e5cb121885
Shareena Begum <shareena.fsp@gmail.com>
2021-08-11 12:27:55 +0200

    handling events (onClick, onMouseover)

--- pages/reactstate.js
+++ pages/reactstate.js
@@ -1,31 +1,47 @@
 import React from "react"
 
-// Given a stateless functional component, add state to it
-// state should have a property called `isLoggedIn` which is a boolean
-// (true if logged in, false if not)
-// Then, give your best shot at rendering the word "in" if the user is logged in
-// or "out" if the user is logged out.
+// // Given a stateless functional component, add state to it
+// // state should have a property called `isLoggedIn` which is a boolean
+// // (true if logged in, false if not)
+// // Then, give your best shot at rendering the word "in" if the user is logged in
+// // or "out" if the user is logged out.
 
-class Reactstate extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            isLoggedIn: false
-        }
-    }
-    render(){
-        let wordDisplay
-        if(this.state.isLoggedIn === true){
-          wordDisplay = "in"  
-        } else{
-          wordDisplay = "out"
-        }
-        return(
-            <div>
-                <h1>You are currently logged {wordDisplay}</h1>
-            </div>
-        )
-    }
+// class Reactstate extends React.Component{
+//     constructor(){
+//         super()
+//         this.state = {
+//             isLoggedIn: true
+//         }
+//     }
+//     render(){
+//         let wordDisplay
+//         if(this.state.isLoggedIn === true){
+//           wordDisplay = "in"  
+//         } else{
+//           wordDisplay = "out"
+//         }
+//         return(
+//             <div>
+//                 <h1>You are currently logged {wordDisplay}</h1>
+//             </div>
+//         )
+//     }
+// }
+
+function handleClick(){
+    console.log("I was clicked!")
+}
+
+function Reactstate(){
+    return(
+        <div>
+            <img onMouseOver={() => console.log("Hovered!")}
+             src="https://www.fillmurray.com/300/200" />
+            <br />
+            <br />
+            <button onClick={handleClick}>Click me</button>
+        </div>
+    )
 }
 
 
