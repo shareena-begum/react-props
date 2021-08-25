80c20ecabe6dd7eb814916efbf41318d5b844265
Shareena Begum <shareena.fsp@gmail.com>
2021-08-25 10:26:04 +0200

    form part-2

--- pages/form.js
+++ pages/form.js
@@ -5,21 +5,22 @@ class form extends Component{
         super()
         this.state = {
            firstName: "",
-           lastName: ""
+           lastName: "",
+           isFriendly: false,
+           gender: "",
+           favColor: "blue"
         }
         this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(event) {
-        const {name, value} = event.target
-        this.setState({
-            [name]: value
-        }) 
+        const {name, value, type, checked} = event.target
+        type === "checkbox" ? this.setState( {[name]: checked} ) : this.setState( {[name]: value} ) 
     }
 
     render() { 
      return(
-        <form>
+        <form onSubmit={this.handleSubmit}>
             <input 
                 type="text" 
                 value={this.state.firstName} 
@@ -35,7 +36,58 @@ class form extends Component{
                 placeholder="Last Name" 
                 onChange={this.handleChange} 
                 />
+
+            <textarea value={"some default value"} 
+            onChange={this.handleChange} 
+                />
+            <label>
+                <input
+                    type="checkbox"
+                    name="isFriendly"
+                    checked={this.state.isFriendly} 
+                    onChange={this.handleChange}
+                    /> Is Friendly?
+            </label>  
+
+            <label>
+                <input
+                    type="radio"
+                    name="gender"
+                    value="male"
+                    checked={this.state.gender === "male"} 
+                    onChange={this.handleChange}
+                    /> Male
+            </label>  
+
+            <label>
+                <input
+                    type="radio"
+                    name="gender"
+                    value="female"
+                    checked={this.state.gender === "female"} 
+                    onChange={this.handleChange}
+                    /> Female
+            </label> 
+            {/* Formik */}
+            <br />
+
+        <label>Favorite Color:</label>
+            <select 
+                value={this.state.favColor}
+                onChange={this.handleChange}
+                name="favColor"
+                >
+                    <option value="blue">Blue</option>
+                    <option value="green">Green</option>
+                    <option value="red">Red</option>
+                    <option value="yellow">Yellow</option>
+                    <option value="orange">Orange</option>
+            </select>
+
             <h1>{this.state.firstName} {this.state.lastName}</h1>
+            <h2>You are a {this.state.gender}</h2>
+            <h2>Your Favorite Color is {this.state.favColor}</h2>
+            <button>Submit</button>
         </form>
     )
   }
