99ce3872f8e851191b660675ee806368128fb960
Shareena Begum <shareena.fsp@gmail.com>
2021-08-31 21:03:33 +0200

    counter app with useeffect hook

--- pages/hooks.js
+++ pages/hooks.js
@@ -1,90 +1,90 @@
 import React, {useState} from "react"
 
-function hooks() {
-    const [inputData, setInputData] = useState({firstName: "", lastName: "", checked: "male", checked: "female" })
-    const [contactsData, setContactsData] = useState([])
+// function hooks() {
+//     const [inputData, setInputData] = useState({firstName: "", lastName: "", checked: "male", checked: "female" })
+//     const [contactsData, setContactsData] = useState([])
     
-    function handleChange(event) {
-        const {name, value, checked} = event.target
-        setInputData(prevInputData =>  ({...prevInputData, [name]: value}))
-    }
-
-    function handleSubmit(event) {
-        event.preventDefault()
-        setContactsData(prevContacts => [...prevContacts, inputData])
-    }
-    
-    const contacts = contactsData.map(contact => <h2 key={contact.firstName + contact.lastName}>{contact.firstName} {contact.lastName}</h2>)
-
-    return (
-        <>
-        <form onSubmit={handleSubmit}>
-            <input
-                placeholder="First Name"
-                name="firstName" 
-                value= {inputData.firstName}
-                onChange={handleChange}
-                />
-                <br/>
-
-            <input
-                placeholder="Last Name"
-                name="lastName" 
-                value= {inputData.lastName}
-                onChange={handleChange}
-                />
-                <br />
-                <label>
-                <input
-                    type="radio"
-                    name="gender"
-                    value="male"
-                    checked={inputData.male} 
-                    onChange={handleChange}
-                    /> Male
-            </label>  
-             <br/>
-            <label>
-                <input
-                    type="radio"
-                    name="gender"
-                    value="female"
-                    checked={inputData.female} 
-                    onChange={handleChange}
-                    /> Female
-            </label> 
-            <br />
-
-                <button>Add Contact</button>
-        </form>
-        {contacts}
-        </>
-    )
-}
-
-
-
-
-
-// function hooks(){
-//     const [count, setCount] = useState(0)
-
-//     function increment() {
-//         setCount(prevCount => prevCount + 1)
+//     function handleChange(event) {
+//         const {name, value, checked} = event.target
+//         setInputData(prevInputData =>  ({...prevInputData, [name]: value}))
 //     }
 
-//     function decrement() {
-//         setCount(prevCount => prevCount - 1)
+//     function handleSubmit(event) {
+//         event.preventDefault()
+//         setContactsData(prevContacts => [...prevContacts, inputData])
 //     }
+    
+//     const contacts = contactsData.map(contact => <h2 key={contact.firstName + contact.lastName}>{contact.firstName} {contact.lastName}</h2>)
 
-//     return(
-//         <div className="hooks">
-//             <h1>{count}</h1>
-//             <button onClick= {increment}>INCREMENT!</button> <br />
-//             <button onClick= {decrement}>DECREMENT! </button>
-//         </div>
+//     return (
+//         <>
+//         <form onSubmit={handleSubmit}>
+//             <input
+//                 placeholder="First Name"
+//                 name="firstName" 
+//                 value= {inputData.firstName}
+//                 onChange={handleChange}
+//                 />
+//                 <br/>
+
+//             <input
+//                 placeholder="Last Name"
+//                 name="lastName" 
+//                 value= {inputData.lastName}
+//                 onChange={handleChange}
+//                 />
+//                 <br />
+//                 <label>
+//                 <input
+//                     type="radio"
+//                     name="gender"
+//                     value="male"
+//                     checked={inputData.male} 
+//                     onChange={handleChange}
+//                     /> Male
+//             </label>  
+//              <br/>
+//             <label>
+//                 <input
+//                     type="radio"
+//                     name="gender"
+//                     value="female"
+//                     checked={inputData.female} 
+//                     onChange={handleChange}
+//                     /> Female
+//             </label> 
+//             <br />
+
+//                 <button>Add Contact</button>
+//         </form>
+//         {contacts}
+//         </>
 //     )
 // }
+
+
+
+
+
+function hooks(){
+    const [count, setCount] = useState(0)
+
+    function increment() {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    function decrement() {
+        setCount(prevCount => prevCount - 1)
+    }
+
+    return(
+        <div className="hooks">
+            <h1>{count}</h1>
+            <button onClick= {increment}>INCREMENT!</button> <br />
+            <button onClick= {decrement}>DECREMENT! </button>
+        </div>
+    )
+}
 
 
 // function hooks() {
