d0b61d71511e3b86764da280e17c8b31a0b370df
Shareena Begum <shareena.fsp@gmail.com>
2021-09-02 13:15:53 +0200

    React movie search app with basic styling and addition of form

---
+++ pages/movie.js
@@ -0,0 +1,14 @@
+import React from "react"
+import SearchMovies from "../src/components/Movie/SearchMovies"
+
+ function movie() {
+     return (
+         <div className="container">
+             <h1 className="title">React Movie Search</h1>
+             <SearchMovies/>
+         </div>
+     )
+ }
+
+
+export default movie
---
+++ src/components/Movie/SearchMovies.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+ function SearchMovies() {
+    return (
+        <form className="form">
+            <label className="label" htmlFor="query">Movie Name</label>
+            <input className="input" type="text" name="query"
+            placeholder="i.e. Jurassic Park"  />
+            <button className="button" type="submit">Search</button>
+        </form>
+    )
+}
+
+export default SearchMovies
