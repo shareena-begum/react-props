bf0aa6baf4042c6e27b9e1b5219f1fb64014948d
Shareena Begum <shareena.fsp@gmail.com>
2021-09-06 22:22:08 +0200

    cooking blog on react

---
+++ pages/projectone.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react"
+import Header from "../src/components/ProjectOne/Header"
+import Main from "../src/components/ProjectOne/Main"
+import Footer from "../src/components/ProjectOne/Footer"
+
+class projectone extends Component {
+    constructor(){
+        super()
+        this.state = {
+            
+        }
+    }
+
+
+       
+    
+    render() {
+        return (
+            <div className="container ">
+               <Header />
+            </div>
+        )
+    }
+}
+
+
+
+
+
+export default projectone
