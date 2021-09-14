5b56fd703baacf9476a7c25f4eb7e5abeff7c19e
Shareena Begum <shareena.fsp@gmail.com>
2021-09-14 20:23:58 +0200

    dietaryform

---
+++ src/components/Dietary/DietaryForm.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+class DietaryForm extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <form>
+                <label></label>
+                <input />
+            </form>
+        )
+    }
+}
+
+export default DietaryForm
