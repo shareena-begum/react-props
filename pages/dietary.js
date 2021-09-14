cdb1f82981889c594fe88ebdef506aad63a21ce6
Shareena Begum <shareena.fsp@gmail.com>
2021-09-14 20:18:30 +0200

    dietary blog

---
+++ pages/dietary.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function dietary() {
+    return(
+        <div>
+            <h3>It's a dietary products blog</h3>
+        </div>
+       
+    )
+}
+
+
+
+
+export default dietary
