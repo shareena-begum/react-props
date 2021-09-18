83c23c323dc0911c7cb0bd89890f50c81c28f516
shareena-begum <84287797+shareena-begum@users.noreply.github.com>
2021-09-18 19:24:57 +0200

    basic info of the dairy form

--- src/components/Dairy/DairyForm.js
+++ src/components/Dairy/DairyForm.js
@@ -5,18 +5,41 @@ class DairyForm extends Component {
         super()
         this.state = {
             name: ""
+            
         }
     }
     render() {
         return(
-            <form>
+            <div>
+                <h4>Registration Form:</h4>
+                <p>Wants to share your knowledge of diary farming, then your just a 
+                    step back to get yourself into the world of S.M. Dairy Farm. 
+                    Before you register read the following requirements</p>
+                <ul>
+                    <li>Can communicate with cows, buffaloes, sheeps</li>
+                    <li>Can wash and clean them</li>
+                    <li>Can squeeze the milk</li>
+                    <li>Can make food for animals</li>
+                </ul>
+                <form>
                 <label>Name:</label>
                 <input 
+                value={this.state.name}
                  type="text"
                  name="Full Name"
                  placeholder="full name"
+
+                 />
+                 <br />
+                 <label>Email:</label>
+                <input 
+                 type="text"
+                 name="Email id"
+                 placeholder="email"
                  />
             </form>
+            </div>
+            
         )
     }
 }
