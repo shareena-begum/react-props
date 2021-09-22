2a42372ea57125c79392a1cbb9b5f3fb40527e8b
shareena-begum <84287797+shareena-begum@users.noreply.github.com>
2021-09-22 15:42:19 +0200

    react practice.2 deleted all files and made one function callout

--- pages/callout.js
+++ pages/callout.js
@@ -1,30 +1,31 @@
 import React from "react"
-import InfoCallout from "../src/components/ReactChildren/InfoCallout"
-import ImageCallout from "../src/components/ReactChildren/ImageCallout"
-import EmailCallout from "../src/components/ReactChildren/EmailCallout"
+
+import Callout from "../src/components/ReactChildren/Callout"
 
 function callout (){
     return(
         <main>
             <h1>Welcome!</h1>
-            <InfoCallout 
-                header="Don't miss out!"
-                body="Unless you don't suffer from FOMO, you better make sure you fill out the email form below!" 
-            />
+            <Callout>
+                <h2>Don't miss out!</h2>
+                <p>Unless you don't suffer from FOMO, you better make sure you fill out the email form below!</p>
+            </Callout>
             
             <p>This is probably the best site you've ever come across. I'm glad you're here to witness the magnificence of this website right now.</p>
             
-            <ImageCallout 
-                img={"https://picsum.photos/id/102/4320/3240"} 
-                caption="Just look at those sparkling raspberries!"
-            />
+            <Callout>
+                <img src="https://picsum.photos/id/102/4320/3240" width="100%" />
+                <figcaption>Just look at those sparkling raspberries!</figcaption>
+            </Callout>
             
             <p>Here's some more unforgettable content. Lorem ipsum something or other.</p>
+
+            <Callout>
+                <h2>Give us your email. We definitely won't sell it to anyone.</h2>
+                <input type="email" placeholder="Enter Email"/>
+                <button>Sign me up!</button>
+            </Callout>
             
-            <EmailCallout 
-                header="Give us your email. We definitely won't sell it to anyone." 
-                btnText="Sign me up!" 
-            />
             
         </main>
     )
