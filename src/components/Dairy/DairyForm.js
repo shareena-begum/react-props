0c75ec84e8ddd5704e1e94b22e65277dc47f92cb
shareena-begum <84287797+shareena-begum@users.noreply.github.com>
2021-09-23 20:35:59 +0200

    checkbox, radio, select are added

--- src/components/Dairy/DairyForm.js
+++ src/components/Dairy/DairyForm.js
@@ -4,9 +4,18 @@ class DairyForm extends Component {
     constructor(){
         super()
         this.state = {
-            name: ""
+            fullname: "",
+            emailId: "",
+            experienced: false,
+            gender: "",
+            experience: "Above 5 years"
             
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(event){
+        const {name, value, type, checked} = event.target
+        type === "checkbox" ? this.setState({ [name] : checked }) : this.setState({ [name] : value })
     }
     render() {
         return(
@@ -21,22 +30,72 @@ class DairyForm extends Component {
                     <li>Can squeeze the milk</li>
                     <li>Can make food for animals</li>
                 </ul>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                 <label>Name:</label>
-                <input 
-                value={this.state.name}
+                <input
                  type="text"
-                 name="Full Name"
-                 placeholder="full name"
+                 value={this.state.fullname}
+                 onChange={this.handleChange}
+                 placeholder="Full Name"
 
                  />
                  <br />
                  <label>Email:</label>
                 <input 
                  type="text"
-                 name="Email id"
-                 placeholder="email"
+                 value={this.state.emailId}
+                 placeholder="Email Id"
+                 onChange={this.handleChange}
                  />
+
+                 <br />
+                   <label>
+                 <input 
+                 type= "checkbox"
+                 name="experienced"
+                 checked={this.state.experienced}
+                 onChange={this.handleChange}
+                 /> Experienced in a nimal husbandry
+                    </label> 
+                 <br />
+
+                 <label>For How Many Years:</label>
+                 <select
+                 value={this.state.experience}
+                 onChange={this.handleChange}
+                 name="experience"
+                 >
+                     <option value="below 1 year">Below 1 Year</option>
+                     <option value="1-5 years">1-5 Years</option>
+                     <option value="above 5 years">Above 5 Years</option>
+                 </select>
+
+                 <br />
+
+                 <label> 
+                 <input 
+                 type= "radio"
+                 name="gender"
+                 value="male"
+                 checked={this.state.gender === "male"}
+                 onChange={this.handleChange}
+                 />Male
+                 </label> 
+
+                 <label> 
+                 <input 
+                 type= "radio"
+                 name="gender"
+                 value="female"
+                 checked={this.state.gender === "female"}
+                 onChange={this.handleChange}
+                 />Female
+                </label> 
+
+                <br />
+
+                <button>Submit</button>
+
             </form>
             </div>
             
