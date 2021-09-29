1bce76942ec471221abd058c41c6350b5913212c
shareena-begum <84287797+shareena-begum@users.noreply.github.com>
2021-09-29 16:33:44 +0200

    renderprop challange (Datafetcher from API)

--- pages/renderprops.js
+++ pages/renderprops.js
@@ -3,20 +3,52 @@ import RenderProps from "../src/components/HOC/RenderProps"
 
 function renderprops() {
   return (
-      <div>
-          <RenderProps render={
-            function(bool, number) {
-              return (
-                <div>
-                  <h1> {number} </h1>
-                  <h1> {bool ? "true" : "false"} </h1>
-                </div>
-              )
-            }
-            }/>
-      </div>
+    <div>
+      <RenderProps url="https://swapi.dev/api/people/1">
+        {({data, loading}) => {
+          return (
+            loading?
+            <h1>Loading...</h1> :
+            <p>{JSON.stringify(data)}</p>
+          )
+        }}
+      </RenderProps>
+    </div>
   )
 }
 
 
+
+// function renderprops() {
+//   return (
+//       <div>
+//           <RenderProps render={
+//             function(bool, number) {
+//               return (
+//                 <div>
+//                   <h1> {number} </h1>
+//                   <h1> {bool ? "true" : "false"} </h1>
+//                 </div>
+//               )
+//             }
+//             }/>
+//       </div>
+//   )
+// }
+
+
 export default renderprops
+
+{/**
+                 * Part 2: Call the function the DataFetcher is expecting.
+                 * If should receive the data and the loading state, and return the JSX
+                 * that makes use of that info. If the data is still loading, display
+                 * "Loading..." in an h1 element, and once the data has loaded, just display 
+                 * the data with `<p>{JSON.stringify(data)}</p>`
+                 * 
+                 * Remember: With the render props pattern, you can use a custom prop
+                 * (typically called `render`), OR you can use `props.children`. Based
+                 * on what's already written here for you, you should be able to figure
+                 * out which of these we're using. (You may have to make changes to the
+                 * DataFetcher component based on what you see here.)
+                 */}
--- src/components/HOC/RenderProps.js
+++ src/components/HOC/RenderProps.js
@@ -1,11 +1,51 @@
-import React from "react"
+import React, {Component} from "react"
 
-function RenderProps(props) {
-    return (
-        <div>
-           {props.render(true, 36)}
-        </div> 
-    ) 
+class RenderProps extends Component {
+    state = {
+        loading: false,
+        data: null
+    }
+
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch(this.props.url)
+        .then(res => res.json())
+        .then(data => this.setState({data, loading: false}))
+    }
+
+    render() {
+        const {data, loading} = this.state
+        return (
+            this.props.children({data, loading})
+        )
+    }
 }
 
+
+// function RenderProps(props) {
+//     return (
+//         <div>
+//            {props.render(true, 36)}
+//         </div> 
+//     ) 
+// }
+
 export default RenderProps
+
+// renderprops challange is datafetcher from Api
+/**
+ * Challenge: Fill in the blanks in the code here (part 1) and in the App.js
+ * file (part 2) until the code is working.
+ * 
+ * Render props are meant to allow us to re-use functionalty in multiple places.
+ * If your app were never to become more complicated than what we have here, it's
+ * probably not worth the effort to create a separate data fetching component.
+ * 
+ * HOWEVER, if you did find yourself repeating these things (setting a loading state,
+ * returning data, etc.) over and over in your app, a component like this might 
+ * make sense to build.
+ * 
+ * Tip: you can use your browser's dev tools to inspect the full React errors 
+ * in the console. Click in the menu bar above the editor to access the real
+ * right-click menu, or use your browser's shortcut code to get to the dev tools.
+ */
