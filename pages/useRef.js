4c3efb788266247376569f3688cc33fd8db7efcd
shareena-begum <84287797+shareena-begum@users.noreply.github.com>
2021-10-22 21:31:16 +0200

    useRef hooks

---
+++ pages/useRef.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react"
+
+function useRef() {
+    const [newTodoValue, setNewTodoValue] = useState("")
+    const [todosList, setTodosList] = useState([])
+    
+    function handleChange(event) {
+        setNewTodoValue(event.target.value)
+    }
+    
+    function addTodo(event) {
+        event.preventDefault()
+        setTodosList(prevTodosList => [...prevTodosList, newTodoValue])
+        setNewTodoValue("")
+    }
+    
+    const allTodos = todosList.map(todo => <p key={todo}>{todo}</p>)
+    
+    return (
+        <div>
+            <form>
+                <input type="text" name="todo" value={newTodoValue} onChange={handleChange}/>
+                <button onClick={addTodo}>Add todo item</button>
+            </form>
+            {allTodos}
+        </div>
+    )
+}
+
+export default useRef
